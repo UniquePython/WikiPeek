9aa5a8d425ee9c2f8dd9de9d470bdb281f1ffb2d
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-20 12:24:16 +0530

    parse the wiki data

--- src/ingest.py
+++ src/ingest.py
@@ -1,8 +1,31 @@
+import bz2
+import json
 import re
+import xml.etree.ElementTree as ET
 
 
 def parse_dump(dump_path: str, output_path: str) -> None:
-    pass
+    NS = "{http://www.mediawiki.org/xml/export-0.11/}"
+
+    with bz2.open(dump_path, "rb") as f, open(output_path, "w") as out:
+        doc_id = 0
+        for event, element in ET.iterparse(f, events=("end",)):
+            if element.tag == f"{NS}page":
+                ns = element.findtext(f"{NS}ns")
+                title = element.findtext(f"{NS}title")
+                text = element.findtext(f"{NS}revision/{NS}text")
+                element.clear()
+
+                if ns != "0" or not text:
+                    continue
+
+                text = extract_text(text)
+                record = {"id": doc_id, "title": title, "text": text}
+                out.write(json.dumps(record) + "\n")
+                doc_id += 1
+
+                if doc_id % 10_000 == 0:
+                    print(f"{doc_id} articles processed...")
 
 
 def extract_text(raw_wiki_text: str) -> str:
@@ -11,8 +34,17 @@ def extract_text(raw_wiki_text: str) -> str:
         r"\{\{.*?\}\}", "", raw_wiki_text, flags=re.DOTALL
     )  # remove {{...}} templates
     raw_wiki_text = re.sub(
-        r"\[\[(?:[^|\]]*\|)?([^\]]+)\]\]", r"\1", raw_wiki_text, flags=re.DOTALL
-    )  # get the part after | from [[...|...]]
+        r"\[\[(?:File|Image):[^\]]*\]\]",
+        "",
+        raw_wiki_text,
+        flags=re.IGNORECASE | re.DOTALL,
+    )  # ignore file / image links
+    raw_wiki_text = re.sub(
+        r"\[\[(?:[^\]|]*\|)*([^\]|]+)\]\]", r"\1", raw_wiki_text
+    )  # handles [[a]], [[a|b]], [[a|b|c|d]] -> always takes last segment
+    raw_wiki_text = re.sub(
+        r"thumb\|[\w|]+\|", "", raw_wiki_text
+    )  # remove image thumbnail directives
     raw_wiki_text = re.sub(
         r"<.*?>", "", raw_wiki_text, flags=re.DOTALL
     )  # remove HTML tags
@@ -26,8 +58,11 @@ def extract_text(raw_wiki_text: str) -> str:
         r"'{2,3}(.*?)'{2,3}", r"\1", raw_wiki_text
     )  # get the part inside ''...'' and '''...'''
     raw_wiki_text = re.sub(r"\s{2,}", " ", raw_wiki_text)  # collapse whitespace
+    raw_wiki_text = re.sub(
+        r"^\*+\s*", "", raw_wiki_text, flags=re.MULTILINE
+    )  # remove bullet points
     return raw_wiki_text.strip()
 
 
 if __name__ == "__main__":
-    parse_dump("data/simplewiki-latest-pages-articles.xml.bz2", "documents.jsonl")
+    parse_dump("data/simplewiki-latest-pages-articles.xml.bz2", "data/documents.jsonl")
